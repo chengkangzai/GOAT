7d3a944ef269ea96e37dfecff197a2d66246e9cc
Ching Cheng Kang <43839286+chengkangzai@users.noreply.github.com>
2022-12-04 15:26:00 +0800

    integrate with firebase hosting

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+    reactStrictMode: true,
+    swcMinify: true,
+    // images: {
+    //     unoptimized: true
+    // }
+    images: {
+        loader: "imgix",
+        path: "https://noop/",
+    }
 }
 
 module.exports = nextConfig
